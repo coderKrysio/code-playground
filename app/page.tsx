53c616af30d7289905046df08b271ad893e6af7d
Anushree <anushreejaiswal.srn@gmail.com>
2023-07-07 22:03:42 +0530

    added navbarg

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      hi
+      <Navbar />
     </main>
   );
 }
