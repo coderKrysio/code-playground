7e081faffeffbbcfc5b2c7b9e768fd3ba9a431cd
Anushree <anushreejaiswal.srn@gmail.com>
2023-07-07 22:13:37 +0530

    used prettier

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: 'plus.unsplash.com',
-        },
-        ],
-    },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
