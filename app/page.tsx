de62fa44728f50579fdeacd665274b817c4bd1a1
Anushree <anushreejaiswal.srn@gmail.com>
2023-07-07 21:59:02 +0530

    project setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
-import Navbar from "@/components/Navbar";
-import { Carousel } from "react-responsive-carousel";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <Navbar />
-      <Carousel />
+      hi
     </main>
   );
 }
--- components/Carousel.tsx
+++
@@ -1,9 +0,0 @@
-const Carousel = () => {
-  return (
-    <div className="border-2 border-white w-screen mt-[60px] text-white">
-      hi
-    </div>
-  );
-};
-
-export default Carousel;
