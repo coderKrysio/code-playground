be1ab51a52d283d46cfd1151cffc22f68d2d3a32
Anushree <anushreejaiswal.srn@gmail.com>
2023-12-28 02:16:26 +0530

    updated navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,9 @@
-import { Black_Ops_One } from "next/font/google";
-
-const gameFont = Black_Ops_One({
-  subsets: ["latin"],
-  weight: "400",
-});
-
 export const Navbar = () => {
-  return (
-    <div className="h-[60px] w-screen flex justify-start items-center p-5 fixed top-0 left-0 z-[10]">
-      <h1 className={`${gameFont.className} text-3xl text-[#2cb67d]`}>
-        JS Games
-      </h1>
-    </div>
-  );
+    return (
+        <div className="h-[50px] w-screen flex justify-start items-center p-5 fixed top-0 left-0 z-[10] bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10">
+            <h1 className="text-2xl font-semibold tracking-wide text-[#00ff9f]">
+                Multi Jeux
+            </h1>
+        </div>
+    );
 };
