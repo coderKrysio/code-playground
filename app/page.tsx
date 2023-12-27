159feba383e4c8b6e39502e05d2091a6f8fb4487
Anushree <anushreejaiswal.srn@gmail.com>
2023-12-28 02:18:17 +0530

    updated components import

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import { Navbar } from "@/components/navbar";
-import { CarouselContainer } from "@/components/carousel-container";
-import { CategoryPanel } from "@/components/category-panel";
-
+import { HeroComponent } from "@/components/hero-component";
+import { GameListing } from "@/components/game-listing";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <Navbar />
-      <CarouselContainer />
-      <CategoryPanel />
-      <CategoryPanel />
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between overflow-hidden bg-[#091833]">
+            <Navbar />
+            <HeroComponent />
+            <GameListing />
+        </main>
+    );
 }
