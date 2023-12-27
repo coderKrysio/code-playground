e9250cea8e0b990ae31254a874dc3853d5d3df15
Anushree <anushreejaiswal.srn@gmail.com>
2023-12-28 02:24:39 +0530

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import { Navbar } from "@/components/navbar";
 import { HeroComponent } from "@/components/hero-component";
 import { GameListing } from "@/components/game-listing";
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between overflow-hidden bg-[#091833]">
-            <Navbar />
+            <div className="h-[50px] w-screen flex justify-start items-center p-5 fixed top-0 left-0 z-[10] bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10">
+                <h1 className="text-2xl font-semibold tracking-wide text-[#00ff9f]">
+                    Multi Jeux
+                </h1>
+            </div>
             <HeroComponent />
             <GameListing />
         </main>
