73a65c3e488136176906ae36fe8ab9ceef73ae65
Anushree <anushreejaiswal.srn@gmail.com>
2024-12-29 20:38:22 +0530

    added site data

---
+++ utils/data.ts
@@ -0,0 +1,90 @@
+export const games = [
+    {
+        id: 1,
+        name: "Rock Paper Scissor",
+        image: "/game1.png",
+        github: "https://github.com/coderKrysio/rock-paper-scissor",
+        link: "https://the-rock-paper-scissor-game.vercel.app/",
+    },
+
+    {
+        id: 2,
+        name: "Colour Ball Blast",
+        image: "/game2.png",
+        github: "https://github.com/coderKrysio/ColourBallBlast",
+        link: "https://colourballblast.netlify.app/",
+    },
+    {
+        id: 3,
+        name: "Dots & Lines",
+        image: "/game3.png",
+        github: "https://github.com/coderKrysio/DotsAndLines.git",
+        link: "https://dotsandlines.netlify.app/",
+    },
+    {
+        id: 4,
+        name: "Falling Ball",
+        image: "/game4.png",
+        github: "https://github.com/coderKrysio/FallingBall",
+        link: "https://fallingballbyanushree.netlify.app/",
+    },
+    {
+        id: 5,
+        name: "Connect Four",
+        image: "/game5.png",
+        github: "https://github.com/coderKrysio/ConnectFour.git",
+        link: "https://connectfourbyanushree.netlify.app/",
+    },
+    {
+        id: 6,
+        name: "Brick Ball",
+        image: "/game6.png",
+        github: "https://github.com/coderKrysio/BrickBallGame.git",
+        link: "https://brickballbyanushree.netlify.app/",
+    },
+    {
+        id: 7,
+        name: "Asteroids",
+        image: "/game7.png",
+        github: "https://github.com/coderKrysio/AsteroidsGame.git",
+        link: "https://asteroidsgamebyanushree.netlify.app/",
+    },
+    {
+        id: 8,
+        name: "Randome Meme Generator",
+        image: "/game8.png",
+        github: "https://github.com/coderKrysio/meme-generator",
+        link: "https://random-meme-generator-v1.netlify.app/",
+    },
+];
+
+export const websites = [
+    {
+        id: 1,
+        name: "GDGC Club Website",
+        image: "/site1.png",
+        github: "https://github.com/GDGCVITBHOPAL/gdgc-website",
+        link: "https://gdsc-website-2024.vercel.app/",
+    },
+    {
+        id: 2,
+        name: "Campus Shield",
+        image: "/site2.png",
+        github: "https://github.com/coderKrysio/campus-shield",
+        link: "https://vit-campus-shield.vercel.app/",
+    },
+    {
+        id: 3,
+        name: "Biodiversity Compass",
+        image: "/site3.png",
+        github: "https://github.com/coderKrysio/biodiversity-compass",
+        link: "https://biodiversity-compass.vercel.app/",
+    },
+    {
+        id: 4,
+        name: "GDSCxVITBhopal",
+        image: "/site4.png",
+        github: "https://github.com/coderKrysio/GDSCxVITBhopal",
+        link: "https://gdscxvitbhopal.vercel.app/",
+    },
+];
