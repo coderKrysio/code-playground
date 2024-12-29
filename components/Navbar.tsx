aa8080c45a50da9577ac85ab3cf295c2221c9b1b
Anushree <anushreejaiswal.srn@gmail.com>
2024-12-29 20:38:41 +0530

    updated navbar styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,8 @@
 export const Navbar = () => {
     return (
-        <div className="h-[50px] w-screen flex justify-start items-center p-5 fixed top-0 left-0 z-[10] bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10">
-            <h1 className="text-2xl font-semibold tracking-wide text-[#00ff9f]">
-                Multi Jeux
+        <div className="h-[50px] w-screen flex justify-start items-center p-5 fixed top-0 left-0 z-[10] bg-[#050e1f]">
+            <h1 className="text-xl font-semibold tracking-wide text-[#00ff9f]">
+                Code Playground
             </h1>
         </div>
     );
