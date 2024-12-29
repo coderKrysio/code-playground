888f83a7f69acdaff25501cb5e140665ce9cba1e
Anushree <anushreejaiswal.srn@gmail.com>
2024-12-29 20:40:50 +0530

    updated landing page components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import { Footer } from "@/components/Footer";
 import { HeroComponent } from "@/components/Hero";
 import { ListingGame } from "@/components/Listing-Games";
+import { ListingSites } from "@/components/Listing-Sites";
 import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between overflow-hidden bg-[#091833]">
+        <main className="flex min-h-screen flex-col items-center justify-between p-4 overflow-hidden bg-[#091833]">
             <Navbar />
-            <HeroComponent />
             <ListingGame />
+            <ListingSites />
             <Footer />
         </main>
     );
